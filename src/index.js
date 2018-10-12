0fda6a8433d9aaebc7539d8714828d33f0d8a9d0
Justin Mayfield <tooker@gmail.com>
2018-10-11 19:11:44 -0600

    Copy some boilerplate from vault codebase.

---
+++ src/index.js
@@ -0,0 +1,72 @@
+
+const process = require('process');
+const relay = require('librelay');
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const vaultApi = require('./vault-api');
+const path = require('path');
+const process = require('process');
+
+const root = `${__dirname}/../../dist`;
+const build = require(`${root}/build.json`);
+const port = process.env.PORT || '4096';
+
+let _rejectCount = 0;
+
+process.on('unhandledRejection', ev => {
+    console.error(ev);
+    if (_rejectCount++ > 100) {
+        console.error("Reject count too high, killing process.");
+        process.exit(1);
+    }
+});
+
+
+
+class WebServer {
+
+    constructor(bot) {
+        const jsenv = {};
+        for (const key of Object.keys(build)) {
+            jsenv[key.toUpperCase()] = build[key];
+        }
+        this.bot = bot;
+        this.port = port;
+        this.app = express();
+        if (process.env.HEROKU_FORCE_SSL) {
+            console.log('forcing SSL on heroku');
+            this.app.use(require('heroku-ssl-redirect')());
+        }
+        this.app.use(morgan('dev')); // logging
+        this.app.use(bodyParser.json());
+        this.app.use('/api/vault/', (new vaultApi.VaultAPIv1({server: this})).router);
+        this.app.use('/api/onboard/', (new api.OnboardAPIV1({server: this})).router);
+        this.app.use('/api/auth/', (new api.AuthenticationAPIV1({server: this})).router);
+        this.app.use('/static/', express.static(path.join(root, 'static'), {strict: true}));
+        this.app.get('/env.js', (req, res) => {
+            res.setHeader('Content-Type', 'application/javascript');
+            res.send(`self.F = self.F || {}; F.env = ${JSON.stringify(jsenv)};\n`);
+        });
+        this.app.get('/*', (req, res) => res.sendFile('static/html/index.html', {root}));
+        this.app.use((req, res, next) => {
+            res.status(404).json({
+                error: 'bad_request',
+                message: 'not_found'
+            });
+        });
+        this.app.use((error, req, res, next) => {
+            res.status(500).json({
+                error: 'server',
+                message: error
+            });
+        });
+    }
+
+    async start() {
+        this.app.listen(this.port, process.env.LISTEN_ADDR);
+    }
+}
+
+module.exports = WebServer;
