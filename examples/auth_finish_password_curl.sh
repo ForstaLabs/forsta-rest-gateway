82798d8923d88700992866bd3943a14988e6c21b
Justin Mayfield <tooker@gmail.com>
2018-12-13 14:01:20 -0700

    Add auth finish examples (sms and password)

---
+++ examples/auth_finish_password_curl.sh
@@ -0,0 +1,1 @@
+curl -v -X PUT http://localhost:8086/auth/v1/@foo:bar -H 'content-type: application/json' -d '{"password": "SECRET", "type": "password"}'
---
+++ examples/auth_finish_sms_curl.sh
@@ -0,0 +1,1 @@
+curl -v -X PUT http://localhost:8086/auth/v1/@foo:bar -H 'content-type: application/json' -d '{"code": "123456", "type": "sms"}'
